75987ae26c989c671eb48f1528aba4fa8ece92c2
SmarthSachdeva <101968647+SmarthSachdeva@users.noreply.github.com>
2025-02-12 15:23:09 +0530

    Time: 1 ms (90.14%) | Memory: 41.7 MB (61.04%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -2,12 +2,14 @@ class Solution {
     public int countGoodSubstrings(String s) {
         int cnt = 0;
         
-        // Use a sliding window approach
         for (int i = 0; i <= s.length() - 3; i++) {
-            String sub = s.substring(i, i + 3);  // Get a substring of length 3
-            // Check if all characters in the substring are unique
-            if (sub.charAt(0) != sub.charAt(1) && sub.charAt(0) != sub.charAt(2) && sub.charAt(1) != sub.charAt(2)) {
-                cnt++;  // Count the substring if all characters are unique
+            String sub = s.substring(i, i + 3); 
+            if (    sub.charAt(0) != sub.charAt(1) && 
+                    sub.charAt(0) != sub.charAt(2) && 
+                    sub.charAt(1) != sub.charAt(2)
+                ) {
+                    
+                cnt++; 
             }
         }
         
